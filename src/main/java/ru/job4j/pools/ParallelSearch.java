77f3db72a56a3933c99155d3692dcd5f648c5c4e
AleevRustam <re1973@mail.ru>
2025-03-14 10:34:33 +0200

    3. ForkJoinPool [#315067 #535875]

--- src/main/java/ru/job4j/pools/ParallelSearch.java
+++ src/main/java/ru/job4j/pools/ParallelSearch.java
@@ -1,25 +1,8 @@
 package ru.job4j.pools;
 
-import java.util.concurrent.ForkJoinPool;
-
 public class ParallelSearch<T extends Comparable<T>> {
 
     public int search(T[] array, T target) {
-        if (array.length <= SearchIndex.ARRAY_LIMIT) {
-            return linearSearch(array, target);
-        } else {
-            ForkJoinPool forkJoinPool = new ForkJoinPool();
-            return forkJoinPool.invoke(new SearchIndex<>(array, target, 0, array.length));
-
-        }
-    }
-
-    private int linearSearch(T[] array, T target) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].compareTo(target) == 0) {
-                return i;
-            }
-        }
-        return -1;
+           return SearchIndex.search(array, target);
     }
 }
--- src/main/java/ru/job4j/pools/SearchIndex.java
+++ src/main/java/ru/job4j/pools/SearchIndex.java
@@ -20,22 +20,31 @@ public class SearchIndex<T extends Comparable<T>> extends RecursiveTask<Integer>
     @Override
     protected Integer compute() {
         if (to - from <= ARRAY_LIMIT) {
-            for (int i = from; i < to; i++) {
-                if (array[i].compareTo(target) == 0) {
-                    return i;
-                }
-            }
-            return -1;
-        } else {
-            int middle = (from + to) / 2;
-            SearchIndex<T> left = new SearchIndex<>(array, target, from, middle);
-            SearchIndex<T> right = new SearchIndex<>(array, target, middle, to);
-
-            left.fork();
-            int rightResult = right.compute();
-            int leftResult = left.join();
-
-            return leftResult != -1 ? leftResult : rightResult;
+            return linearSearch();
         }
+        int middle = (from + to) / 2;
+        SearchIndex<T> left = new SearchIndex<>(array, target, from, middle);
+        SearchIndex<T> right = new SearchIndex<>(array, target, middle, to);
+
+        left.fork();
+        int rightResult = right.compute();
+        int leftResult = left.join();
+
+        return leftResult != -1 ? leftResult : rightResult;
+
+    }
+
+    private int linearSearch() {
+        for (int i = from; i < to; i++) {
+            if (array[i].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static <T extends Comparable<T>> int search(T[] array, T target) {
+        SearchIndex<T> task = new SearchIndex<>(array, target, 0, array.length);
+        return task.compute();
     }
 }
